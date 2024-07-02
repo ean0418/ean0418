eeff0b9b62deb1d6350c620b2d16e01872106359
ean0418 <abhjs1337@naver.com>
2024-07-02 17:41:34 +0900

    Add files via upload

---
+++ Jul02_2_Spring_DI/src/main/java/com/ean/jul022/dog/Dog.java
@@ -0,0 +1,33 @@
+package com.ean.jul022.dog;
+
+public class Dog {
+	private String name;
+	private int age;
+	
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Dog(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	 
+}
